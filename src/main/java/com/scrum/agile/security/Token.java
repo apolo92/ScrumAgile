601d16792c64f13491cfd3545822fe6a61e3df3e
Agustin Becerra Mila <agustin.becerra@ext.aqualogybs.net>
2016-08-11 11:07:59 +0200

    Token: sign token with label - TODO: Change string to json

--- src/main/java/com/scrum/agile/security/Token.java
+++ src/main/java/com/scrum/agile/security/Token.java
@@ -1,7 +1,46 @@
 package com.scrum.agile.security;
 
+import com.auth0.jwt.JWTSigner;
+
+import java.util.HashMap;
+
 /**
- * Created by apolo on 11/08/2016.
+ * Created by apolo & agustibm on 11/08/2016.
  */
 public class Token {
+
+    private String password;
+    private final HashMap<String, Object> info;
+    private final HashMap<String, Object> token;
+    private long expires;
+    private long initTokenDate;
+
+    private JWTSigner tokenSigner;
+    private String infoToken;
+
+
+    public Token(String password) {
+        this.token = new HashMap<String, Object>();
+        this.info = new HashMap<String, Object>();
+        this.password = password;
+        this.tokenSigner = new JWTSigner(password);
+        this.infoToken = "";
+    }
+
+    public String sign(){
+        this.token.put("iss", this.infoToken);
+        this.token.put("initTokenDate", this.initTokenDate);
+        this.token.put("exp", this.expires);
+        return tokenSigner.sign(this.token);
+    }
+
+    public void setExpires(long expires) {
+        this.initTokenDate = System.currentTimeMillis() / 1000L;
+        this.expires = initTokenDate + expires;
+    }
+
+    public void addInfo(String info) {
+        //TODO: Change String to JSON OBject
+        this.infoToken += info;
+    }
 }
--- src/test/java/TokenTest.java
+++ src/test/java/TokenTest.java
@@ -1,11 +1,25 @@
+import com.scrum.agile.security.Token;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
+
 /**
- * Created by apolo on 11/08/2016.
+ * Created by apolo & agustibm on 11/08/2016.
  */
 public class TokenTest {
 
     @Test
-    public void testTravis(){
+    public void get_token(){
+        Token token = new Token("password");
+        token.setExpires(60L);
+        token.addInfo("Test");
+
+        String tokenForSend = token.sign();
+
+        String expectedToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9";
+        String infoFromToken = tokenForSend.split("\\.")[0];
+        assertEquals(expectedToken, infoFromToken);
+
     }
 }
