ceb37704573c336d5f930e26b4216ae0bf2dcd49
Alejandro Polo Rangel <alejandro.polo@ext.aqualogybs.net>
2016-08-11 09:08:53 +0200

    Structure security token

---
+++ src/main/java/com/scrum/agile/security/Token.java
@@ -0,0 +1,7 @@
+package com.scrum.agile.security;
+
+/**
+ * Created by apolo on 11/08/2016.
+ */
+public class Token {
+}
---
+++ src/test/java/TokenTest.java
@@ -0,0 +1,5 @@
+/**
+ * Created by apolo on 11/08/2016.
+ */
+public class TokenTest {
+}
